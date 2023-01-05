645d1fcb7f2b233e1a10bdad0aa1ddcb0d2fe16b
Imtiaz Tamim <itamim12202@gmail.com>
2023-01-05 20:47:00 +0600

    Add solution for problem `A - Greatest Convex` Link: https://codeforces.com/contest/1768/problem/A Tags: greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1768/submission/188062659

--- codeforces/1768/A.cpp
+++ codeforces/1768/A.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int a;
-        cin >> a;
-        cout << a - 1 << endl;
-    }
+    int a;
+    cin >> a;
 
-    return 0;
+    while (a--)
+    {
+        int b;
+        cin >> b;
+
+        if (b < 1)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << b - 1 << endl;
+        }
+    }
 }
